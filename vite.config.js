fa77ba7031973d8e5da5906d2dca2a653af46fa6
mdfaizan2101 <mohdfaizan21012002@gmail.com>
2025-05-11 23:30:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
   plugins: [react(), ghPages()],
-  base: '/your-repo-name/', // ⬅️ important: match GitHub repo name
+  base: '/Softsell/', // ⬅️ important: match GitHub repo name
 });
